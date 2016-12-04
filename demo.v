7fc0524dc7e3804af50dcebcd408a83f464c2ee1
Hengruo Zhang <hengruo@ruoMBP.local>
2016-12-05 00:40:36 +0830

    modified:   build 	modified:   demo.v

--- demo.v
+++ demo.v
@@ -3,12 +3,12 @@ Declare ML Module "myplugin".
 Theorem trans_imply_3: forall P1 P2 P3:Prop,
 (P1 -> P2) -> (P2 -> P3) -> (P1 -> P3).
 Proof.
-myplugin.
+killtrans.
 Qed.
 
 Theorem trans_imply_4: forall P1 P2 P3 P4,
 (P1 -> P2) -> (P2 -> P3) -> (P3 -> P4) -> (P1 -> P4).
 Proof.
-myplugin.
+killtrans.
 Qed.
 
